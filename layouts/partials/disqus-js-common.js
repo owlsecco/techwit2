52b3f3a91c7e2b92c0d660fc1b46591def0e5590
Stephen Ajulu <alunje73@gmail.com>
2020-08-19 10:10:42 +0300

    Update disqus-js-common.js

--- layouts/partials/disqus-js-common.js
+++ layouts/partials/disqus-js-common.js
@@ -3,4 +3,4 @@ var disqusButton = document.getElementById('disqus-button');
 disqusButton.parentNode.removeChild(disqusButton); 
 // Un-hide comments
 var disqusComments = document.getElementById('disqus-comments');
-disqusComments.style.display = 'block';
+disqusComments.style.display = 'block'; 
